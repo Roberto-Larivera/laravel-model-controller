6a3c269f197ad26d2d0bf7f1c72d13d24f0da1cd
Roberto Larivera <robylariv@gmail.com>
2023-03-11 01:08:43 +0100

    Preparazione progetto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app') {{-- estende in layout  --}}
+
+@section('head_title') Home @endsection {{-- segnaposto dentro head  --}}
+
+@section('body_main') {{-- segnaposto dentro body  --}}
+    
+<main>
+    <h1>
+        Main
+    </h1>
+</main>
+
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources/img/favicon.ico') }}" /> --}}
+    <title>@yield('head_title','App')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- Includiamo gli assets con la direttiva @vite --}}
+    @vite('resources/js/app.js')
+</head>
+<body>
+    @yield('body_css')
+
+    @include('partials.header')
+
+    @yield('body_main')
+
+    @include('partials.footer')
+
+    @yield('body_js')
+</body>
+</html>
